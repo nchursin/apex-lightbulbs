9d5e85febc1c4ec790c4b2a075705b491ba20c39
Nikita Chursin <nchursin@aquivalabs.com>
2017-10-19 18:30:52 +0300

    CodeAction sample

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const vscode = require('vscode');
 const { getLineText } = require('./helpers')
 const { getAvaliableActions } = require('./actions/analyzer')
+const { DOC_SELECTOR } = require('./constants')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,6 +13,14 @@ function activate(context) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "apex-intention-actions" is now active!');
 
+  let codeActionProvider = {
+    provideCodeActions: () => {
+      console.log('CODE ACTION RAN!')
+    }
+  }
+
+  const prov = vscode.languages.registerCodeActionsProvider(DOC_SELECTOR, codeActionProvider)
+  context.subscriptions.push(prov)
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
