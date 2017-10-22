cef87d84a664fb95c7aebe540e666e455b6a99e7
Nikita Chursin <nchursin@aquivalabs.com>
2017-10-22 10:46:47 +0300

    Logger

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 const R = require('ramda')
+const { PLUGIN_NAME } = require('../constants')
 
 const getLineText = R.curry(
   (doc, lineNumber) => doc.getText(doc.lineAt(lineNumber).range).trim()
@@ -13,9 +14,24 @@ const dropFirstWord = (t) => t.replace(firstWord(t), '').trim()
 
 const capitalize = (text) => `${text.charAt(0).toUpperCase()}${text.slice(1)}`
 
+const logger = () => {
+  const oldLog = console.log
+  const oldError = console.error
+  const filename = R.join(':', R.split('/', R.last(R.split(PLUGIN_NAME, __filename))))
+  console.log = function() {
+    Array.prototype.unshift.call(arguments, `${PLUGIN_NAME}::`)
+    oldLog.apply(this, arguments)
+  }
+  console.error = function() {
+    Array.prototype.unshift.call(arguments, `${PLUGIN_NAME}::`)
+    oldError.apply(this, arguments)
+  }
+}
+
 module.exports = {
   getLineText,
   firstWord,
   dropFirstWord,
   capitalize,
+  logger,
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 const R = require('ramda')
-const { getLineText } = require('./helpers')
-const { getCodeLineType, getDefnMetadata } = require('./intentions/analyzer')
+const { getLineText, logger } = require('./helpers')
+const { getDefnMetadata } = require('./intentions/analyzer')
 const { DOC_SELECTOR, ACTION_MAPPING, PLUGIN_NAME } = require('./constants')
 const { getCodeActions, actions } = require('./intentions/actions')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(ctx) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+  // Logger
+  // TODO: find better way to log things
+  logger()
+
   console.log('Congratulations, your extension "apex-intention-actions" is now active!')
 
   const codeActionProvider = {
@@ -19,7 +21,7 @@ function activate(ctx) {
       const text = getLineText(doc, range.start.line)
       try {
         const metadata = getDefnMetadata(text)
-        const result = getCodeActions(ACTION_MAPPING[metadata.defnType], [ text, metadata ])
+        const result = getCodeActions( ACTION_MAPPING[metadata.defnType], [ text, metadata ] )
         return result
       } catch (e) {
         console.log('e >> ', e)
