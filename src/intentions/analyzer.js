ffac37ff2d8df78c5d02fee172668916c917059b
Nikita Chursin <nchursin@aquivalabs.com>
2017-10-22 11:14:34 +0300

    Class metadata gatherer

--- src/intentions/analyzer.js
+++ src/intentions/analyzer.js
@@ -1,6 +1,6 @@
 const R = require('ramda')
 const { getLineText, firstWord, dropFirstWord } = require('../helpers')
-const { REGEX } = require('../constants')
+const { REGEX, TYPES, CLASS_TYPES, SHARING_TYPES } = require('../constants')
 
 const isComplexType = R.contains('<')
 
@@ -34,7 +34,7 @@ const getVarMetadata = (t) => {
   }
   let varName = firstWord(text)
   if (varName.endsWith(';')) {
-    varName = varName.slice(0, -1);
+    varName = varName.slice(0, -1)
   }
   text = dropFirstWord(text)
 
@@ -46,14 +46,56 @@ const getVarMetadata = (t) => {
   }
 }
 
+const getClassMetadata = (t) => {
+  let text = t
+  const access = firstWord(text)
+  text = dropFirstWord(text)
+  let sharing, classType
+  if ('class' !== firstWord(text) && !R.startsWith('with', firstWord(text))) {
+    if (firstWord(text) === 'abstract') {
+      classType = CLASS_TYPES.ABSTRACT
+    } else {
+      classType = CLASS_TYPES.VIRTUAL
+    }
+    text = dropFirstWord(text)
+  }
+  if ('class' !== firstWord(text) && R.startsWith('with', firstWord(text))) {
+    if (firstWord(text) === 'with') {
+      sharing = SHARING_TYPES.WITH_SHARING
+    } else {
+      sharing = SHARING_TYPES.WITHOUT_SHARING
+    }
+    text = dropFirstWord(dropFirstWord(text))
+  }
+  text = dropFirstWord(text)
+  const className = firstWord(text)
+  if (!sharing) {
+    sharing = SHARING_TYPES.INHERIT
+  }
+  if (!classType) {
+    classType = CLASS_TYPES.NORMAL
+  }
+  return {
+    access,
+    sharing,
+    classType,
+    className,
+  }
+}
+
 const getDefnMetadata = (t) => {
   const defnType = getType(t)
   let metadata = {
     defnType,
   }
   switch (defnType) {
+    case TYPES.CLASS:
+      metadata = R.merge(metadata, getClassMetadata(t))
+      console.log('class metadata >> ', metadata)
+      break;
     default:
       metadata = R.merge(metadata, getVarMetadata(t))
+      console.log('var metadata >> ', metadata)
   }
   return metadata
 }
