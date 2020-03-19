9a4e3daaae04d2ae0780bcec9dfe9c94612a6725
Nikita Chursin <chursinn91@gmail.com>
2020-03-19 22:17:12 +0300

    refactored meta extraction

--- src/lib/lineType.ts
+++ src/lib/lineType.ts
@@ -1,5 +1,5 @@
 import { TYPES } from '../constants';
-import { join, tail } from 'ramda';
+import { join, tail, find } from 'ramda';
 import { types } from 'util';
 
 const modifiers = [
@@ -22,6 +22,19 @@ export const getLineMetadata = (lineText: string): LineMetadata => {
     return result;
 };
 
+const metaChecks = [
+    {
+        regex: varRegex,
+        type: TYPES.VAR,
+        staticKeywordGroupNumber: 4,
+    },
+    {
+        regex: methodRegex,
+        type: TYPES.METHOD,
+        staticKeywordGroupNumber: 4,
+    },
+];
+
 export class LineMetadata {
     public type: string;
     public isStatic: boolean | undefined;
@@ -29,17 +42,11 @@ export class LineMetadata {
     constructor(lineText: string) {
         this.type = TYPES.UNKNOWN;
         const lowerCase = lineText.toLowerCase();
-        const isVar = varRegex.test(lowerCase);
-        if (isVar) {
-            const matches = lowerCase.match(varRegex);
-            this.type = TYPES.VAR;
-            this.isStatic = Boolean(matches && matches[staticKeywordGroupNumber]);
-        } else {
-            if (methodRegex.test(lowerCase)) {
-                this.type = TYPES.METHOD;
-                const matches = lowerCase.match(methodRegex);
-                this.isStatic = Boolean(matches && matches[staticKeywordGroupNumber]);
-            }
+        const config = find((metaCheckConfig) => metaCheckConfig.regex.test(lowerCase), metaChecks);
+        if (config) {
+            this.type = config.type;
+            const matches = lowerCase.match(config.regex);
+            this.isStatic = Boolean(matches && matches[config.staticKeywordGroupNumber]);
         }
     }
 }
