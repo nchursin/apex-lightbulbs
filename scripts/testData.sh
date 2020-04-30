ce358483231ce2e131538421d866dcafc8fec248
Nikita Chursin <chursinn91@gmail.com>
2020-04-30 10:34:38 +0300

    added module-alias (unused yet) and renamed test folder from lineType to utils

--- scripts/testData.sh
+++ scripts/testData.sh
@@ -1,4 +1,4 @@
-cp -r test/suite/lib/lineType/data out/test/suite/lib/lineType/data
+cp -r test/suite/lib/utils/data out/test/suite/lib/utils/data
 cp -r test/suite/lib/actionProviders/vars/getterSetterActionProvider/data out/test/suite/lib/actionProviders/vars/getterSetterActionProvider/data
 cp -r test/suite/lib/actionProviders/classes/addConstructor/data out/test/suite/lib/actionProviders/classes/addConstructor/data
 cp -r test/data out/test/data
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import 'module-alias/register';
 
 import { GetterSetterActionProvider } from './lib/actionProviders/vars';
 import { AddConstructorProvider } from './lib/actionProviders/classes';
